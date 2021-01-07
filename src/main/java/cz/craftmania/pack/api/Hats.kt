ff32c8c93c69d678470c511159279853b922d419
MrWakeCZ <joseff.kral@gmail.com>
2021-01-07 20:43:39 +0100

    Přidány všechny itemy z 1.0.0 Resource Packu

--- src/main/java/cz/craftmania/pack/api/Hats.kt
+++ src/main/java/cz/craftmania/pack/api/Hats.kt
@@ -1,17 +1,19 @@
 package cz.craftmania.pack.api
 
+import cz.craftmania.craftcore.spigot.builders.items.ItemBuilder
 import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
 
 enum class Hats(
-        private val id: Int,
-        private val itemName: String,
-        private val material: Material,
-        private val modelData: Int,
-        private val inVersion: String) {
+     private val id: Int,
+     private val itemName: String,
+     private val material: Material,
+     private val modelData: Int,
+     private val inVersion: String) {
 
-    KOALA(1, "§7§lCoala", Material.CARVED_PUMPKIN, 100001, "0.0.2"),
-    PANDA(2, "§f§lPanda", Material.CARVED_PUMPKIN, 100002, "0.0.2"),
-    HORSE(3, "§6§lHorse", Material.CARVED_PUMPKIN, 100003, "0.0.2");
+    KOALA(1, "§7Coala", Material.CARVED_PUMPKIN, 100001, "1.0.0"),
+    PANDA(2, "§fPanda", Material.CARVED_PUMPKIN, 100002, "1.0.0"),
+    HORSE(3, "§6Horse", Material.CARVED_PUMPKIN, 100003, "1.0.0");
 
     fun getName(): String {
         return this.itemName
@@ -27,5 +29,9 @@ enum class Hats(
 
     fun getAddedVersion(): String {
         return this.inVersion;
+    }
+
+    fun getItemStack(): ItemStack {
+        return ItemBuilder(this.material).setName(this.itemName).setCustomModelData(this.modelData).setLore("§7Přidáno v: §f" + this.inVersion).build();
     }
 }
---
+++ src/main/java/cz/craftmania/pack/api/Items.kt
@@ -0,0 +1,51 @@
+package cz.craftmania.pack.api
+
+import cz.craftmania.craftcore.spigot.builders.items.ItemBuilder
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+enum class Items (
+    private val id: Int,
+    private val itemName: String,
+    private val material: Material,
+    private val modelData: Int,
+    private val inVersion: String) {
+
+    // Crates
+    WOODEN_CRATE_V1(1, "§fWooden Crate", Material.FIREWORK_STAR, 100001, "1.0.0"),
+    SILVER_CRATE_V1(2, "§fSilver Crate", Material.FIREWORK_STAR, 100002, "1.0.0"),
+    GOLDEN_CRATE_V1(3, "§fGolden Crate", Material.FIREWORK_STAR, 100003, "1.0.0"),
+    EMERALD_CRATE_V1(4, "§fEmerald Crate", Material.FIREWORK_STAR, 100004, "1.0.0"),
+    DIAMOND_CRATE_V1(5, "§fDiamond Crate", Material.FIREWORK_STAR, 100005, "1.0.0"),
+    OBSIDIAN_CRATE_V1(6, "§fObsidian Crate", Material.FIREWORK_STAR, 100006, "1.0.0"),
+
+    // Tutorial Items
+    TUTORIAL_BOOK(7, "§eTutorial Boook", Material.WHEAT, 100001, "1.0.0"),
+
+    // Gift Boxes
+    GIFT_WAKE_SPECIAL(8, "§9Gift Wake Special", Material.WHEAT, 200001, "1.0.0"),
+    GIFT_BLUE(9, "§bGift Blue", Material.WHEAT, 200002, "1.0.0"),
+    GIFT_RED(10, "§cGift Red", Material.WHEAT, 200003, "1.0.0");
+
+
+    fun getName(): String {
+        return this.itemName
+    }
+
+    fun getMaterial(): Material {
+        return this.material
+    }
+
+    fun getModelData(): Int {
+        return this.modelData
+    }
+
+    fun getAddedVersion(): String {
+        return this.inVersion;
+    }
+
+    fun getItemStack(): ItemStack {
+        return ItemBuilder(this.material).setName(this.itemName).setCustomModelData(this.modelData).setLore("§7Přidáno v: §f" + this.inVersion).build();
+    }
+
+}
