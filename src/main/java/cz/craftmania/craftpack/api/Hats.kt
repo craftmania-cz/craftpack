964efa77aafabf6e5a4ae4691cbcd2b30629cc8f
MrWakeCZ <joseff.kral@gmail.com>
2021-01-20 04:57:54 +0100

    Hats: Změna itemu na Paper

--- src/main/java/cz/craftmania/craftpack/api/Hats.kt
+++ src/main/java/cz/craftmania/craftpack/api/Hats.kt
@@ -11,11 +11,11 @@ enum class Hats(
      private val modelData: Int,
      private val inVersion: String) {
 
-    KOALA(1, "§7Coala", Material.CARVED_PUMPKIN, 100001, "1.0.0"),
-    PANDA(2, "§fPanda", Material.CARVED_PUMPKIN, 100002, "1.0.0"),
-    HORSE(3, "§6Horse", Material.CARVED_PUMPKIN, 100003, "1.0.0"),
-    CLOWN(4, "§dClow", Material.CARVED_PUMPKIN, 100004, "1.0.2"),
-    LAST_BREATH(5, "§bLast Breath", Material.CARVED_PUMPKIN, 500000, "1.0.2");
+    KOALA(1, "§7Coala", Material.PAPER, 100001, "1.0.0"),
+    PANDA(2, "§fPanda", Material.PAPER, 100002, "1.0.0"),
+    HORSE(3, "§6Horse", Material.PAPER, 100003, "1.0.0"),
+    CLOWN(4, "§dClow", Material.PAPER, 100004, "1.0.2"),
+    LAST_BREATH(5, "§bLast Breath", Material.PAPER, 500000, "1.0.2");
 
     fun getItemStack(): ItemStack {
         return ItemBuilder(this.material).setName(this.itemName).setCustomModelData(this.modelData).setLore("§7Přidáno v: §f" + this.inVersion).build();
