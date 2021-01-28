30c124889fd39f6ecb555cf26b34cfe6fabc48c9
MrWakeCZ <joseff.kral@gmail.com>
2021-01-28 02:31:00 +0100

    Update Hats pro 1.1.0

--- src/main/java/cz/craftmania/craftpack/api/Hats.kt
+++ src/main/java/cz/craftmania/craftpack/api/Hats.kt
@@ -14,8 +14,14 @@ enum class Hats(
     KOALA(1, "§7Coala", Material.PAPER, 100001, "1.0.0"),
     PANDA(2, "§fPanda", Material.PAPER, 100002, "1.0.0"),
     HORSE(3, "§6Horse", Material.PAPER, 100003, "1.0.0"),
-    CLOWN(4, "§dClow", Material.PAPER, 100004, "1.0.2"),
-    LAST_BREATH(5, "§bLast Breath", Material.PAPER, 500000, "1.0.2");
+    CLOWN(4, "§dClown", Material.PAPER, 100004, "1.0.2"),
+    UNICORN(5, "§fUnicorn", Material.PAPER, 100005, "1.1.0"),
+    BEER_HAT(6, "§6BeerHat", Material.PAPER, 100006, "1.1.0"),
+    TREX(7, "§6Trex", Material.PAPER, 100007, "1.1.0"),
+    XXXTENCATION(8, "§bXXXTENCATION", Material.PAPER, 100008, "1.1.0"),
+    BUNNY_EARS(9, "§dBunnyEars", Material.PAPER, 100009, "1.1.0"),
+    ZABICKA(10, "§aZabicka", Material.PAPER, 100010, "1.1.0"),
+    LAST_BREATH(50, "§bLast Breath", Material.PAPER, 500000, "1.0.2");
 
     fun getItemStack(): ItemStack {
         return ItemBuilder(this.material).setName(this.itemName).setCustomModelData(this.modelData).setLore("§7Přidáno v: §f" + this.inVersion).build();
