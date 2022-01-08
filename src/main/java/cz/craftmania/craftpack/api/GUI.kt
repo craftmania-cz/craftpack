4abf662e98884952e09665cefea5faee8509c057
MrWakeCZ <joseff.kral@gmail.com>
2022-01-08 22:13:05 +0100

    GUI: Přidán chybějící server selector & Golden Pig

--- src/main/java/cz/craftmania/craftpack/api/GUI.kt
+++ src/main/java/cz/craftmania/craftpack/api/GUI.kt
@@ -11,7 +11,9 @@ enum class GUI (
     private val modelData: Int,
     private val inVersion: String) {
 
-    VOTEPASS(1, "VotePass", Material.LIGHT_GRAY_STAINED_GLASS_PANE, 100001, "1.2.1");
+    VOTEPASS(1, "VotePass", Material.LIGHT_GRAY_STAINED_GLASS_PANE, 100001, "1.2.1"),
+    SERVER_SELECTOR(2, "Server Selector (lobby)", Material.LIGHT_GRAY_STAINED_GLASS_PANE, 100002, "1.4.0"),
+    GOLDEN_PIG(3, "Golden Pig (Lobby)", Material.LIGHT_GRAY_STAINED_GLASS_PANE, 100003, "1.4.4");
 
     fun getPureItemStack(): ItemStack {
         return ItemBuilder(this.material).setName(this.itemName).setCustomModelData(this.modelData).build();
